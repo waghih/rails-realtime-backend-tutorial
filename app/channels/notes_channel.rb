1042817df4b2309fef4aed2aef3459ffa47b1d25
Farooq Ezhar <waghih.co@gmail.com>
2019-10-23 14:14:26 +0800

    Add notes API

---
+++ app/channels/notes_channel.rb
@@ -0,0 +1,15 @@
+class NotesChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "notes"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def receive(data)
+    note = Note.find(data['id'])
+    note.update!(text: data['text'])
+    ActionCable.server.broadcast('notes', data)
+  end
+end
